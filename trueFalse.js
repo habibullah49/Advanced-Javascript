4709e9632a6c03209850a84a68598e98867b33f9
HP <habibullahsheik640@gmail.com>
2022-01-19 14:38:05 +0600

    true vs false

---
+++ trueFalse.js
@@ -0,0 +1,24 @@
+//falsy
+//0
+//""
+//undefined
+//null
+//NaN
+
+
+//truthy
+
+//'0' ' ' [] {}
+
+
+// const name = "0";
+
+let name = 0;
+console.log(name);
+
+if(name || name == 0){
+    console.log('condition is true');
+}
+else{
+    console.log('Condition is false');
+}
